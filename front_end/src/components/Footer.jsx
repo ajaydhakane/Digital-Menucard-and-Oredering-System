552f0110db1a187513e12556506d5f6a778be96e
ajaydhakane <118625131+ajaydhakane@users.noreply.github.com>
2024-07-17 13:21:36 +0530

    Add files via upload

---
+++ front_end/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+// import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">ZEESHU</div>
+          <div className="right">
+            <p>Restaurant Name</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By B10</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Ajay Dhakane.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ front_end/src/components/Menu1.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Menu1 = () => {
+  return (
+    <div>
+      <h1>hj</h1>
+    </div>
+  )
+}
+
+export default Menu1
